7f08a281b4e7b76bfa207460861aa984a8e79162
samhenrigold <samhenrigold@gmail.com>
2023-12-24 10:04:50 -0500

    throwing stuff at the wall

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,25 @@ import type { Database } from './database';
 dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+app.get('/', async (req: Request, res: Response) => {
+	const supabase = createClient<Database>(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
+
+	let { data: matchup, error: matchupError } = await supabase
+		.from('matchups')
+		// .insert({})
+		.select(`
+		matchup_id,
+		tweet_1:tweet_id1(*, tweet_media(*), in_reply_to_status_id(*, tweet_media(*))),
+		tweet_2:tweet_id2(*, tweet_media(*), in_reply_to_status_id(*, tweet_media(*)))
+	`)
+
+	if (matchupError || !matchup) throw new Error(`Error creating matchup: ${matchupError?.message}`);
+
+	res.setHeader('Content-Type', 'application/json');
+	res.send(JSON.stringify(matchup));
+});
 
 app.get('/get-matchups', async (req: Request, res: Response) => {
 	const supabase = createClient<Database>(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
