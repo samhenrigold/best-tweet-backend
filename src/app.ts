abe3caa4760dfde21b3dabc67e8cb87d0b24e06e
samhenrigold <samhenrigold@gmail.com>
2023-12-24 12:12:23 -0500

    refactoring, added more packages to fix safety

--- src/app.ts
+++ src/app.ts
@@ -1,79 +1,63 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import { createClient } from '@supabase/supabase-js';
 import dotenv from 'dotenv';
-import { VoteData } from './VoteData';
-import bodyParser, { json } from 'body-parser';
+import { json } from 'body-parser';
+import { celebrate, Joi, errors } from 'celebrate';
+import cors from 'cors';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
 
-import type { Database } from './database';
+import { getMatchup, castBallot, validateEnv } from './utils'; // Refactored utility functions
 
 dotenv.config();
+validateEnv(); // Validate necessary environment variables
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(bodyParser.json());
+app.use(cors()); // Enable CORS
+app.use(helmet()); // Set security headers
+app.use(json()); // Body parser
+app.use(rateLimit({ // Rate limiting
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100 // limit each IP to 100 requests per windowMs
+}));
 
-const supabase = createClient<Database>(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
 
-function respondWithError(res: Response, error: string, statusCode: number = 500) {
-    res.setHeader('Content-Type', 'application/json');
-    res.status(statusCode).send(JSON.stringify({ error }));
-}
-
-// GET /get-matchup
-app.get('/get-matchups', async (req: Request, res: Response) => {
-	let { data: matchup, error: matchupError } = await supabase
-		.from('matchups')
-		.insert({})
-		.select(`
-			matchup_id,
-			tweet_1:tweet_id1_str(*, tweet_media(*), users(*), in_reply_to_status_id(*, tweet_media(*))),
-			tweet_2:tweet_id2_str(*, tweet_media(*), users(*), in_reply_to_status_id(*, tweet_media(*)))
-	`)
-
-	if (matchupError || !matchup) throw new Error(`Error creating matchup: ${matchupError?.message}`);
-
-	res.setHeader('Content-Type', 'application/json');
-	res.send(JSON.stringify(matchup));
+app.get('/get-matchups', async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		const matchup = await getMatchup(supabase);
+		res.json(matchup);
+	} catch (error) {
+		next(error);
+	}
 });
 
-// POST: /cast-ballot. Accepts a JSON body with the VoteData interface.
-app.post('/cast-ballot', async (req: Request, res: Response) => {
-    const voteData: any = req.body;
-
-	console.log("Received selected_tweet_id:", voteData.selected_tweet_id);
-
-    // Validate if the selected_tweet_id exists in the tweets table
-    let { data: tweetExists, error: tweetError } = await supabase
-        .from('matchups')
-        .select('tweet_id1_str, tweet_id2_str')
-
-    // Check if the selected_tweet_id exists in the matchup
-	if (tweetError || !tweetExists) return respondWithError(res, tweetError?.message);
-	if (!tweetExists[0].tweet_id1_str.includes(voteData.selected_tweet_id) && !tweetExists[0].tweet_id2_str.includes(voteData.selected_tweet_id)) {
-		return respondWithError(res, 'Selected tweet does not exist in the matchup', 400);
+app.post('/cast-ballot', celebrate({ // Validate input
+	body: Joi.object({
+		selected_tweet_id: Joi.string().required(),
+		matchup_id: Joi.string().required()
+	})
+}), async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		const voteData = req.body;
+		const vote = await castBallot(supabase, voteData, req.ip);
+		res.json(vote);
+	} catch (error) {
+		next(error);
 	}
+});
 
+// Celebrate error handler
+app.use(errors());
 
-	const { data: vote, error: voteError } = await supabase
-		.from('votes')
-		.insert({
-			matchup_id: voteData.matchup_id,
-			selected_tweet_id_str: voteData.selected_tweet_id
-		} as any)
-		.select()
-
-	res.setHeader('Content-Type', 'application/json');
-	if (voteError || !vote) {
-		if (voteError?.message.includes('duplicate key value')) {
-			return respondWithError(res, 'Vote already exists', 409);
-		}
-		return respondWithError(res, voteError?.message);
-	};
-
-	res.send(JSON.stringify(vote));
+// Global error handler
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+	console.error(err.stack);
+	res.status(500).send({ error: 'Internal Server Error' });
 });
 
 app.listen(port, () => {
-	return console.log(`Express is listening at http://localhost:${port}`);
+	console.log(`Express is listening at http://localhost:${port}`);
 });
---
+++ src/utils.ts
@@ -0,0 +1,65 @@
+import { SupabaseClient } from '@supabase/supabase-js';
+
+// Environment Variable Validation
+export function validateEnv() {
+    if (!process.env.SUPABASE_URL || !process.env.SUPABASE_ANON_KEY) {
+        throw new Error("Missing necessary environment variables: SUPABASE_URL or SUPABASE_ANON_KEY");
+    }
+}
+
+// Retrieve Matchup Data
+export async function getMatchup(supabase: SupabaseClient) {
+    let { data: matchup, error } = await supabase
+        .from('matchups')
+        .insert({})
+        .select(`
+            matchup_id,
+            tweet_1:tweet_id1_str(*, tweet_media(*), users(*), in_reply_to_status_id(*, tweet_media(*))),
+            tweet_2:tweet_id2_str(*, tweet_media(*), users(*), in_reply_to_status_id(*, tweet_media(*)))
+        `);
+
+    if (error || !matchup) {
+        throw new Error(`Error retrieving matchup: ${error?.message}`);
+    }
+
+    return matchup;
+}
+
+// Cast Ballot
+export async function castBallot(supabase: SupabaseClient, voteData: any, userIp: string) {
+    // Validate if the selected_tweet_id exists in the tweets table
+    let { data: tweetExists, error: tweetError } = await supabase
+        .from('matchups')
+        .select('tweet_id1_str, tweet_id2_str')
+        .eq('matchup_id', voteData.matchup_id);
+
+    console.log(tweetExists);
+
+    if (tweetError || !tweetExists) {
+        throw new Error(tweetError?.message);
+    }
+
+    let exists = tweetExists.some(tweet => 
+        tweet.tweet_id1_str == String(voteData.selected_tweet_id) || 
+        tweet.tweet_id2_str == String(voteData.selected_tweet_id)
+    );
+
+    if (!exists) {
+        throw new Error('Selected tweet does not exist in the matchup');
+    }
+
+    const { data: vote, error: voteError } = await supabase
+        .from('votes')
+        .insert({
+            matchup_id: voteData.matchup_id,
+            selected_tweet_id_str: voteData.selected_tweet_id,
+            user_ip: userIp
+        })
+        .select();
+
+    if (voteError || !vote) {
+        throw new Error(voteError?.message);
+    }
+
+    return vote;
+}
